2bfbf2a4e2dd019922a5dff35fc91e6eef4ddcdf
Dejan Ristic <idejanristic@gmail.com>
2021-01-17 18:27:43 +0100

    fixed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,8 @@ class DashboardController extends Controller
 {
     public function chart()
     {
+        Gate::authorize('view', 'orders');
+        
         $orders = Order::query()
             ->join('order_items', 'orders.id', '=', 'order_items.order_Id')
             ->selectRaw('DATE_FORMAT(orders.created_at, "%Y-%m-%d") as date, sum(order_items.quantity * order_items.price) as sum')
